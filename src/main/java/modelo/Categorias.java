2ae0986268855d494d476760b7c636ae77b83be9
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2024-03-20 12:36:56 +0100

    Configuracion y funcionamiento alta categoria

---
+++ src/main/java/modelo/Categorias.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Miguel Angel
+ */
+
+@Entity
+@Table(name="categorias")
+public class Categorias implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int idCategoria;
+    
+    @Column(name="nombre")
+    private String nombreCategoria;
+    
+    @Column(name="estado")
+    private boolean estado;
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombreCategoria() {
+        return nombreCategoria;
+    }
+
+    public void setNombreCategoria(String nombreCategoria) {
+        this.nombreCategoria = nombreCategoria;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 89 * hash + this.idCategoria;
+        hash = 89 * hash + Objects.hashCode(this.nombreCategoria);
+        hash = 89 * hash + (this.estado ? 1 : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Categorias other = (Categorias) obj;
+        if (this.idCategoria != other.idCategoria) {
+            return false;
+        }
+        if (this.estado != other.estado) {
+            return false;
+        }
+        if (!Objects.equals(this.nombreCategoria, other.nombreCategoria)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
