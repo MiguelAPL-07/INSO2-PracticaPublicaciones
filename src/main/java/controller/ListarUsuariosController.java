ebc4ed4cd92b994793bd2d1b71cdfdf3008ed434
MiguelAPL-07 <miguelangelpl2020@gmail.com>
2024-05-10 01:48:38 +0200

    Implementando administrar usuarios para administrador

---
+++ src/main/java/controller/ListarUsuariosController.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import EJB.UsuarioFacadeLocal;
+import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import modelo.Usuario;
+
+/**
+ *
+ * @author Miguel Ángel
+ */
+
+@Named
+@RequestScoped
+public class ListarUsuariosController implements Serializable {
+    
+    @EJB
+    private UsuarioFacadeLocal usuarioEJB;
+    
+    private List<Usuario> usuarios;
+    
+    private Usuario usuario;
+    
+    @PostConstruct
+    public void init() {
+        usuarios = usuarioEJB.findAll();
+    }
+    
+    public void establecerUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    public void eliminarUsuario(Usuario usuario) {
+        try{
+            usuarioEJB.remove(usuario);
+            usuarios = usuarioEJB.findAll();
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Eliminación correcta", "Usuario eliminado con éxito"));
+        }catch(Exception e){
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_FATAL, "Error al eliminar", "Error al eliminar el usuario"));
+            System.out.println("Error al eliminar el usuario "+e.getMessage());
+        }
+    }
+
+    public UsuarioFacadeLocal getUsuarioEJB() {
+        return usuarioEJB;
+    }
+
+    public void setUsuarioEJB(UsuarioFacadeLocal usuarioEJB) {
+        this.usuarioEJB = usuarioEJB;
+    }
+
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 53 * hash + Objects.hashCode(this.usuarioEJB);
+        hash = 53 * hash + Objects.hashCode(this.usuarios);
+        hash = 53 * hash + Objects.hashCode(this.usuario);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ListarUsuariosController other = (ListarUsuariosController) obj;
+        if (!Objects.equals(this.usuarioEJB, other.usuarioEJB)) {
+            return false;
+        }
+        if (!Objects.equals(this.usuarios, other.usuarios)) {
+            return false;
+        }
+        if (!Objects.equals(this.usuario, other.usuario)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
